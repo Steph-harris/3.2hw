d4afe5a2171b6914e048eb786f9b9cef98de9454
steph-harris <steph.harris21@gmail.com>
2015-11-12 23:45:16 -0500

    created home html and js files

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// var isAString = prompt("enter something");
+
+// isAString = parseInt(isAString);
+//   if (isNaN(isAString)){
+//     alert("that's a string!")
+//   } else {
+//     alert("nope, that's a " + typeof isAString)
+// };
+
+
+
+var isAString = prompt("enter something");
+
+isAString = typeof(isAString);
+  if (isNaN(isAString)){
+    alert("that's a string!");
+  } else {
+    alert("nope, that's a " + typeof(isAString))
+  };
+
+// stringFinder();
