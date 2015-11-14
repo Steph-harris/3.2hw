d78e07cfb17cd42f33afbedf9ecac9f56bffa098
steph-harris <steph.harris21@gmail.com>
2015-11-13 23:15:45 -0500

    added js file, added forms to html to run js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,29 @@
-// var isAString = prompt("enter something");
-
-// isAString = parseInt(isAString);
-//   if (isNaN(isAString)){
-//     alert("that's a string!")
-//   } else {
-//     alert("nope, that's a " + typeof isAString)
-// };
-
-
-
-var isAString = prompt("enter something");
-
-isAString = typeof(isAString);
+function stringFinder (){
+  usrAnsw = document.getElementById("usrInput").value; 
+  isAString = parseInt(usrAnsw);
   if (isNaN(isAString)){
-    alert("that's a string!");
+    alert("that's a string!")
   } else {
-    alert("nope, that's a " + typeof(isAString))
-  };
+    alert("nope, that's a " + typeof isAString)
+  }
+};
+//
+function smallNum (firstn, secondn){
+  firstn = prompt("pick a number 1-99");
+  secondn = Math.random() * 100;
+  if (firstn < secondn){
+    console.log(firstn)
+    document.write ("My number is " + secondn + ", Your number is smaller:" + firstn);
+  } else {
+    console.log(secondn)
+    document.write ("My number is " + secondn + ", My number is smaller than yours:" + firstn);
+  }
+};
 
-// stringFinder();
+//Adds n number of strings together
+function concatStr (){
+  addStr = document.getElementById("mix").value || []; 
+  // var allT = addStr.join('')
+  // console.log(allT);
+  alert(addStr.join(''));
+}
